aa0d6c38c92b3a9fa9b8a237b0810bee0d5ffba4
quocphu <duongquocphu2015@gmail.com>
2022-07-12 21:16:15 +0700

    API error

--- src/Slice/movie.js
+++ src/Slice/movie.js
@@ -1,11 +1,28 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieAPI from "../Services/movieAPI";
+
+const initialState = {
+  bannerMovies: [],
+};
 
 const movie = createSlice({
   name: "movie",
-  initialState: {
-    movies: [],
-  },
+  initialState,
   reducers: {},
 });
+
+// thunk action
+export const getBannerMovieShowing = createAsyncThunk(
+  "movie/getBannerMovieShowing",
+  async () => {
+    try {
+      const data = await movieAPI.getBannerMovieShowing();
+ 
+      return data
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export default movie.reducer;
