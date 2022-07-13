551d72ab28a7a3ed5cf16de95cb2dc5685ef994e
quocphu <duongquocphu2015@gmail.com>
2022-07-13 19:50:52 +0700

    API error

--- src/Slice/movie.js
+++ src/Slice/movie.js
@@ -11,7 +11,7 @@ export const getBannerMovieShowing = createAsyncThunk(
   async () => {
     try {
       const { data } = await movieAPI.getBannerMovieShowing();
-
+      console.log(data);
       return { bannerMovies: data };
     } catch (error) {
       console.log(error);
