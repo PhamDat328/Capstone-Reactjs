96063e4a78ff61391966f6c33f804e3a10a0b174
quocphu <duongquocphu2015@gmail.com>
2022-07-27 00:52:46 +0700

    detail

--- src/Pages/Detail/MovieDetail.jsx
+++ src/Pages/Detail/MovieDetail.jsx
@@ -4,15 +4,17 @@ import { useParams } from "react-router-dom";
 import { getMovieShowingDetail } from "../../Slice/movie";
 import Button from "react-bootstrap/Button";
 import dayjs from "dayjs";
-
 import "./MovieDetail.css";
 import MyVerticallyCenteredModal from "./MyVerticallyCenteredModal";
 import { getMovieDetailSchedule } from "../../Slice/cinema";
-
+import { Tabs } from "antd";
+import "antd/dist/antd.css";
 const MovieDetail = () => {
+  const { TabPane } = Tabs;
   const [modalShow, setModalShow] = useState(false);
-  const [cineplex, setCineplex] = useState([]);
+  const [eventKey, setEventKey] = useState("");
   const { movieId } = useParams();
+
   const { movieDetail } = useSelector(
     (state) => state.movieReducer.movieDetail
   );
@@ -20,17 +22,14 @@ const MovieDetail = () => {
     (state) => state.cinemaReducer.movieDetailSchedule
   );
 
-  const cinemaList = movieDetailSchedule.heThongRapChieu;
   const dispatch = useDispatch();
+
+  const cinemaListMovie = movieDetailSchedule.heThongRapChieu;
 
   useEffect(() => {
     dispatch(getMovieShowingDetail(movieId));
     dispatch(getMovieDetailSchedule(movieId));
   }, []);
-
-  const handleTheater = (arrTheater) => {
-    setCineplex(arrTheater);
-  };
 
   return (
     <div className="movieDetail">
@@ -49,7 +48,7 @@ const MovieDetail = () => {
           </div>
           <div className="col-2">
             <div className="text-info-movie">
-              <h2>{movieDetail.tenPhim}</h2>
+              <h2 className="text-white">{movieDetail.tenPhim} </h2>
               <p>120 phút</p>
               <a href="#">
                 <span>Mua vé</span>
@@ -70,57 +69,61 @@ const MovieDetail = () => {
           </div>
         </div>
       </div>
-      <div className=" cinema">
-        <div className="row">
-          <div className="col-1">
-            <div className="cinema-list">
-              {cinemaList?.map((cinema) => {
-                return (
-                  <div key={cinema.maHeThongRap} className="cinema-list-detail">
-                    <button
-                      className="cinema-img"
-                      onClick={() => handleTheater(cinema.cumRapChieu)}
-                    >
-                      <img src={cinema.logo} alt="" />
-                      <p>{cinema.tenHeThongRap}</p>
-                    </button>
+
+      <div className="container ml-72 w-2/3 mt-20 bg-white px-4 py-5">
+        <Tabs tabPosition={"left"}>
+          {cinemaListMovie?.map((cinema, index) => {
+            return (
+              <TabPane
+                key={index}
+                tab={
+                  <div
+                    className="cinema-img"
+                    // onClick={() => handleTheater(cinema.cumRapChieu)}
+                  >
+                    <img src={cinema.logo} alt="" />
+                    <p>{cinema.tenHeThongRap}</p>
                   </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="col-2">
-            {cineplex?.map((theater) => (
-              <div
-                key={theater.maCumRap}
-                className="border-bottom theater-list"
+                }
               >
-                <div className="theater-item">
-                  <img src={theater.hinhAnh} alt="" />
-                  <div className="theater-info">
-                    <p>{theater.tenCumRap}</p>
-                    <span>{theater.diaChi}</span>
+                {cinema.cumRapChieu?.map((theater, index) => (
+                  <div key={index}>
+                    <div className="theater-item" key={theater.maCumRap}>
+                      <img src={theater.hinhAnh} alt="" />
+                      <div className="theater-info">
+                        <p>{theater.tenCumRap}</p>
+                        <span>{theater.diaChi}</span>
+                      </div>
+                    </div>
+                    <div className="showtimes">
+                      <div className="row">
+                        {theater.lichChieuPhim
+                          ?.slice(0, 9)
+                          .map((showtimes, index) => (
+                            <div className="col-sm-4" key={index}>
+                              <button className="showtimeBtn">
+                                <p>
+                                  {dayjs(showtimes.ngayChieuGioChieu).format(
+                                    "DD-MM-YYYY "
+                                  )}
+                                </p>
+                                <h6>-</h6>
+                                <span>
+                                  {dayjs(showtimes.ngayChieuGioChieu).format(
+                                    "HH:mm"
+                                  )}
+                                </span>
+                              </button>
+                            </div>
+                          ))}
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div className="showtimes">
-                  {theater.lichChieuPhim.map((showtimes) => (
-                    <button key={showtimes.maLichChieu} className="showtimeBtn">
-                      <p>
-                        {dayjs(showtimes.ngayChieuGioChieu).format(
-                          "DD-MM-YYYY "
-                        )}
-                      </p>
-                      <h6>-</h6>
-                      <span>
-                        {dayjs(showtimes.ngayChieuGioChieu).format("HH:mm")}
-                      </span>
-                    </button>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+                ))}
+              </TabPane>
+            );
+          })}
+        </Tabs>
       </div>
     </div>
   );
