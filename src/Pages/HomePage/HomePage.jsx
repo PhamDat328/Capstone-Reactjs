468316e354e34d6973b7af722a72eb833d048678
quocphu <duongquocphu2015@gmail.com>
2022-08-02 16:07:22 +0700

    login

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Carousel from "./Carousel";
 import Apps from "./Apps";
 import Footer from "./Footer";
-import { useDispatch } from "react-redux";
+import { useDispatch, uSelector } from "react-redux";
 import { getBannerMovieShowing } from "../../Slice/movie";
 import MovieList from "./MovieList/MovieList";
 import CinemaList from "./Cinema/CinemaList";
@@ -15,9 +15,14 @@ import News from "./News/News";
 const HomePage = () => {
   const dispatch = useDispatch();
 
+  
   useEffect(() => {
     dispatch(getBannerMovieShowing());
   }, []);
+
+  // if(isLoading) {
+  //   return <h1>Loading...</h1>
+  // }
 
   return (
     <div className="app">
