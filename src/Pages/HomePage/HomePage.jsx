439da1fa04f8eaa6173cb3ce0f9569f40a550a1e
quocphu <duongquocphu2015@gmail.com>
2022-08-02 10:04:31 +0700

    purchase

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import Carousel from "./Carousel";
+
 import News from "./News";
+
 import Apps from "./Apps";
+
 import Footer from "./Footer";
+
 import { useDispatch } from "react-redux";
 import { getBannerMovieShowing } from "../../Slice/movie";
 import MovieList from "./MovieList/MovieList";
